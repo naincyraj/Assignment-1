50e60603e304b5eba7fda658f7cf3d41886659b4
naincyraj <34609561+naincyraj@users.noreply.github.com>
2017-12-16 13:07:32 -0800

    Create AplusB.java

---
+++ AplusB.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+
+class Addition 
+{
+  public static void main(String[] args) 
+{
+    Scanner s = new Scanner(System.in);
+    
+int a = s.nextInt();
+    
+int b = s.nextInt();
+
+    int sum=a+b;
+System.out.println(sum);
+  }
+}
